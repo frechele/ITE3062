2a11d3839b26b537110ece0b8ad41d9bebeca027
Junyeong Park <jyp10987@gmail.com>
2022-04-03 13:19:54 +0900

    feat(data): implement report summary

--- project1/timetabler/data/lecture.py
+++ project1/timetabler/data/lecture.py
@@ -2,7 +2,7 @@ from collections import namedtuple
 import pandas as pd
 import os
 import pickle
-from typing import List
+from typing import Dict, List
 
 
 Lecture = namedtuple('Lecture', ['name', 'category'])
@@ -10,6 +10,25 @@ LECTURE_REPORT_ELEMENTS = ['lecture', 'professor', 'assignment_counts', 'assignm
                            'team_project', 'exam_counts', 'exam_level', 'exam_type', 'grade_level']
 LectureReport = namedtuple(
     'LectureReport', LECTURE_REPORT_ELEMENTS)
+
+def _make_mapping_table(labels: List[str]) -> Dict[str, int]:
+    return { k: i for i, k in enumerate(labels) }
+
+ASSIGNMENT_COUNTS_LABELS = ['없음', '보통', '많음']
+ASSIGNMENT_LEVEL_LABELS = ['쉬움', '보통', '어려움', '매우어려움']
+TEAM_PROJECT_LABELS = ['없음', '있음']
+EXAM_COUNTS_LABELS = ['없음', '1번', '2번', '3번+']
+EXAM_LEVEL_LABELS = ['쉬움', '보통', '어려움', '매우어려움']
+EXAM_TYPE_LABELS = ['객관식', '혼합형', '논술형']
+GRADE_LEVEL_LABELS = ['너그러움', '보통', '깐깐함']
+
+ASSIGNMENT_COUNTS_MAPPING = _make_mapping_table(ASSIGNMENT_COUNTS_LABELS)
+ASSIGNMENT_LEVEL_MAPPING = _make_mapping_table(ASSIGNMENT_LEVEL_LABELS)
+TEAM_PROJECT_MAPPING = _make_mapping_table(TEAM_PROJECT_LABELS)
+EXAM_COUNTS_MAPPING = _make_mapping_table(EXAM_COUNTS_LABELS)
+EXAM_LEVEL_MAPPING = _make_mapping_table(EXAM_LEVEL_LABELS)
+EXAM_TYPE_MAPPING = _make_mapping_table(EXAM_TYPE_LABELS)
+GRADE_LEVEL_MAPPING = _make_mapping_table(GRADE_LEVEL_LABELS)
 
 
 class LectureList:
@@ -72,6 +91,13 @@ class LectureReportDB:
 
         self.open(_impl)
 
+    def get_report(self, lecture: str, professor: str) -> LectureReport:
+        with open('database/report_summary.pkl', 'rb') as f:
+            df = pickle.load(f)
+
+        record = df[(df['professor'] == professor) & (df['lecture'] == lecture)].iloc[0]
+        return LectureReport(**record.to_dict())
+
     def open(self, fn, read_only: bool = False):
         if os.path.exists('database/reports.pkl'):
             with open('database/reports.pkl', 'rb') as f:
@@ -85,9 +111,47 @@ class LectureReportDB:
             with open('database/reports.pkl', 'wb') as f:
                 pickle.dump(data, f)
 
+    def summary_reports(self, lecture_list: LectureList):
+        with open('database/reports.pkl', 'rb') as f:
+            df = pickle.load(f)
+
+        df_result = pd.DataFrame(columns=LECTURE_REPORT_ELEMENTS)
+
+        lectures = lecture_list.get_lectures()
+        for lecture in lectures:
+            professors = lecture_list.get_professors_by_lecture(lecture)
+
+            for professor in professors:
+                df_lec_prof = df[(df['lecture'] == lecture) &
+                                 (df['professor'] == professor)]
+
+                if len(df_lec_prof) == 0:
+                    continue
+
+                df_lec_prof.loc[:, 'assignment_counts'] = df_lec_prof.assignment_counts.apply(lambda x: ASSIGNMENT_COUNTS_MAPPING[x])
+                df_lec_prof.loc[:, 'assignment_level'] = df_lec_prof.assignment_level.apply(lambda x: ASSIGNMENT_LEVEL_MAPPING[x])
+                df_lec_prof.loc[:, 'team_project'] = df_lec_prof.team_project.apply(lambda x: TEAM_PROJECT_MAPPING[x])
+                df_lec_prof.loc[:, 'exam_counts'] = df_lec_prof.exam_counts.apply(lambda x: EXAM_COUNTS_MAPPING[x])
+                df_lec_prof.loc[:, 'exam_level'] = df_lec_prof.exam_level.apply(lambda x: EXAM_LEVEL_MAPPING[x])
+                df_lec_prof.loc[:, 'exam_type'] = df_lec_prof.exam_type.apply(lambda x: EXAM_TYPE_MAPPING[x])
+                df_lec_prof.loc[:, 'grade_level'] = df_lec_prof.grade_level.apply(lambda x: GRADE_LEVEL_MAPPING[x])
+
+                result_dict = {
+                    'lecture': lecture,
+                    'professor': professor
+                }
+                for key in LECTURE_REPORT_ELEMENTS[2:]:
+                    result_dict[key] = df_lec_prof[key].mean()
+
+                df_result = df_result.append(result_dict, ignore_index=True)
+                
+        with open('database/report_summary.pkl', 'wb') as f:
+            pickle.dump(df_result, f)
+
 
 if __name__ == '__main__':
     lect_list = LectureList()
-    print(lect_list.df)
+    report_db = LectureReportDB()
 
-    print(lect_list.get_professors_by_lecture('운영체제'))
+    report_db.summary_reports(lect_list)
+    print(report_db.get_report('운영체제', '정형수'))
--- project1/timetabler/wizards/report.py
+++ project1/timetabler/wizards/report.py
@@ -2,6 +2,7 @@ import streamlit as st
 from timetabler.data.lecture import Lecture
 
 from timetabler.data.lecture import LectureList, LectureReport, LectureReportDB
+import timetabler.data.lecture as L
 
 
 def show_report():
@@ -14,20 +15,20 @@ def show_report():
         '교수명', lecture_list.get_professors_by_lecture(lecture_name))
 
     assignment_counts = st.select_slider(
-        label='과제 수', options=['없음', '보통', '많음'])
+        label='과제 수', options=L.ASSIGNMENT_COUNTS_LABELS)
     assignment_level = st.select_slider(
-        label='과제 난이도', options=['쉬움', '보통', '어려움', '매우어려움'])
-    team_project = st.select_slider(label='조별 활동', options=['없음', '있음'])
-    exam_counts = st.select_slider(label='시험 횟수', options=[
-                                   '없음', '1번', '2번', '3번+'])
-    exam_level = st.select_slider(label='시험 난이도', options=[
-                                  '쉬움', '보통', '어려움', '매우어려움'])
-    exam_type = st.select_slider(label='시험 타입', options=['객관식', '혼합형', '논술형'])
-    grade_level = st.select_slider(label='성적', options=['너그러움', '보통', '깐깐함'])
+        label='과제 난이도', options=L.ASSIGNMENT_LEVEL_LABELS)
+    team_project = st.select_slider(
+        label='조별 활동', options=L.TEAM_PROJECT_LABELS)
+    exam_counts = st.select_slider(label='시험 횟수', options=L.EXAM_COUNTS_LABELS)
+    exam_level = st.select_slider(label='시험 난이도', options=L.EXAM_LEVEL_LABELS)
+    exam_type = st.select_slider(label='시험 타입', options=L.EXAM_TYPE_LABELS)
+    grade_level = st.select_slider(label='성적', options=L.GRADE_LEVEL_LABELS)
 
     if st.button('제출'):
         db = LectureReportDB()
         db.add_report(LectureReport(lecture=lecture_name, professor=professor_name, assignment_counts=assignment_counts, assignment_level=assignment_level,
                       team_project=team_project, exam_counts=exam_counts, exam_level=exam_level, exam_type=exam_type, grade_level=grade_level))
+        db.summary_reports(lecture_list)
 
         st.info('수강평을 추가하였습니다.')
