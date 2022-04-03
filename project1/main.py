b5b592d85eefbf2239e316e73ea9178f7bd2f97e
Junyeong Park <jyp10987@gmail.com>
2022-04-03 13:38:19 +0900

    feat(wizard): fully implement report summary page

--- project1/main.py
+++ project1/main.py
@@ -9,12 +9,12 @@ from timetabler.wizards.view_report import show_view_report
 if __name__ == '__main__':
     with st.sidebar:
         MENU_CHOOSED = option_menu('Timetabler', [
-            '시간표 마법사', '수강평 남기기', '수강평 보기'
+            '시간표 마법사', '수강평 남기기', '강의정보'
         ], icons=['kanban', 'book', 'easel'])
 
     if MENU_CHOOSED == '시간표 마법사':
         show_timetable_wizard()
     elif MENU_CHOOSED == '수강평 남기기':
         show_report()
-    elif MENU_CHOOSED == '수강평 보기':
+    elif MENU_CHOOSED == '강의정보':
         show_view_report()
--- project1/timetabler/wizards/view_report.py
+++ project1/timetabler/wizards/view_report.py
@@ -2,13 +2,35 @@ import streamlit as st
 from timetabler.data.lecture import Lecture
 
 from timetabler.data.lecture import LectureList, LectureReport, LectureReportDB
+import timetabler.data.lecture as L
 
 
 def show_view_report():
-    st.title('수강평 남기기')
+    st.title('강의 정보')
 
     lecture_list = LectureList()
+    db = LectureReportDB()
 
     lecture_name = st.selectbox('과목명', lecture_list.get_lectures())
     professor_name = st.selectbox(
         '교수명', lecture_list.get_professors_by_lecture(lecture_name))
+
+    record = db.get_report(lecture_name, professor_name)
+
+    st.write('과제 수 (없음 ↔ 많음)')
+    st.progress(record.assignment_counts / (len(L.ASSIGNMENT_COUNTS_LABELS) - 1))
+
+    st.write('과제 난이도 (쉬움 ↔ 매우어려움)')
+    st.progress(record.assignment_level / (len(L.ASSIGNMENT_LEVEL_LABELS) - 1))
+
+    st.write('시험 횟수 (없음 ↔ 3+)')
+    st.progress(record.exam_counts / (len(L.EXAM_COUNTS_LABELS) - 1))
+
+    st.write('시험 난이도 (쉬움 ↔ 매우어려움)')
+    st.progress(record.exam_level / (len(L.EXAM_LEVEL_LABELS) - 1))
+
+    st.write('시험 타입 (객관식 ↔ 주관식)')
+    st.progress(record.exam_type / (len(L.EXAM_TYPE_LABELS) - 1))
+
+    st.write('성적 (너그러움 ↔ 깐깐함)')
+    st.progress(record.grade_level / (len(L.GRADE_LEVEL_LABELS) - 1))
