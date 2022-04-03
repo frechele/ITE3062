8d9a1026dabc74a2efef933104a4d3fd0489696d
Junyeong Park <jyp10987@gmail.com>
2022-04-03 15:45:52 +0900

    feat(data): create TimeTable class

--- project1/timetabler/wizards/timetable.py
+++ project1/timetabler/wizards/timetable.py
@@ -1,4 +1,11 @@
 import streamlit as st
+import matplotlib.pyplot as plt
+
 
 def show_timetable_wizard():
     st.title('시간표 마법사')
+
+    fig_table = plt.figure(figsize=(1.5, 2))
+    plt_table = fig_table.add_subplot(1, 1, 1)
+
+    st.pyplot(fig_table)
