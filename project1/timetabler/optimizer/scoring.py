fd119bdd740e46ac1c976f6399ba60296785ce12
Junyeong Park <jyp10987@gmail.com>
2022-04-24 11:38:26 +0900

    feat: 시간표 평가기 추가

--- project1/timetabler/optimizer/scoring.py
+++ project1/timetabler/optimizer/scoring.py
@@ -9,16 +9,18 @@ class Evaluator:
         self.lecture_list = lecture_list
         self.report_db = report_db
 
-    def evaluate(self, tt: TimeTable, difficulty: int, team: int, exam_difficulty: int, diversity: int) -> float:
+    def evaluate(self, tt: TimeTable, difficulty: int, team: int, exam_difficulty: int, diversity: int, blank_level: int) -> float:
         assignment_score = 0
         team_project_score = 0
         exam_score = 0
         grade_score = 0
+        blank_score = 0
+        total_range = 0
         category_dist = dict()
 
         for lecture in tt.lectures:
             report = self.report_db.get_report(lecture.lecture, lecture.professor)
-            category = self.lecture_list.get_lecture_category(report.lecture)
+            category = self.lecture_list.get_lecture_category(report.lecture)[0]
 
             assignment_score += report.assignment_counts * (report.assignment_level + 0.5)
             team_project_score += report.team_project
@@ -27,18 +29,38 @@ class Evaluator:
 
             category_dist[category] = category_dist.get(category, 0) + 1
 
+        for day in range(5):
+            if np.sum(tt.table[day]) == 0:
+                continue
+
+            for first_idx in range(48):
+                if tt.table[day, first_idx] >= 0:
+                    break
+
+            for last_idx in range(48):
+                if tt.table[day, -last_idx - 1] >= 0:
+                    break
+
+            count = np.sum(tt.table[day, first_idx:-last_idx])
+            total = 48 - first_idx - last_idx
+            blank_score += total - count
+            total_range += total
+
         jeongong_ratio = category_dist.get('전공', 0) / len(tt.lectures)
         category_dist = np.array(list(category_dist.values())) / len(tt.lectures)
 
-        total_score += (difficulty - 1) * (assignment_score + grade_score) \
+        total_score = (difficulty - 1) * (assignment_score + grade_score) \
                       + (team - 1) * team_project_score \
                       + (exam_difficulty - 1) * exam_difficulty
+        total_score /= len(tt.lectures)
+
+        total_score -= blank_score / total_range * -(blank_level - 1)
 
         if diversity == 0:
             total_score += jeongong_ratio
         elif diversity >= 2:
             total_score -= jeongong_ratio * 0.5
         if diversity == 3:
-            total_score += category_dist.var()    
+            total_score += category_dist.var()
 
         return total_score
--- project1/timetabler/wizards/timetable.py
+++ project1/timetabler/wizards/timetable.py
@@ -1,13 +1,54 @@
 import streamlit as st
 import matplotlib.pyplot as plt
+from matplotlib import font_manager, rc
+from timetabler.data.lecture import LectureList, LectureReportDB
 
-from timetabler.data.time_table import LectureTimeDB
+from timetabler.data.time_table import LectureTimeDB, TimeTable
+from timetabler.optimizer.scoring import Evaluator
 
 
 DIFFICULTY_LEVEL_LABELS = ['안정주의', '중립', '도전적']
 TEAM_PROJECT_LABELS = ['싫어요', '중립', '좋아요']
 EXAM_LEVEL_LABELS = ['쉬운게 좋아요', '중립', '나 빼고 다 못풀게 어려워라!']
 DIST_LEVEL_LABELS = ['전공파이터', '중립', '교양을 쌓을래요', '다양한 경험을 할래요']
+BLANK_LEVEL_LABELS = ['싫어요', '중립', '좋아요']
+
+
+def draw_time_table(tt: TimeTable):
+    fig = plt.figure(figsize=(10, 5.89))
+
+    colors = ['pink', 'lightgreen', 'lightblue', 'wheat', 'salmon', 'orange', 'limegreen', 'plum']
+
+    font = font_manager.FontProperties(fname='/usr/share/fonts/truetype/nanum/NanumGothicCoding.ttf').get_name()
+    rc('font', family=font)
+
+    for lecture_idx, lecture in enumerate(tt.lectures):
+        for time in lecture.times:
+            day = time.day - 0.48 + 1
+            start = time.start / 2
+            end = time.end / 2
+
+            plt.fill_between((day, day+0.96), (start, start), (end, end), color=colors[lecture_idx % len(colors)], edgecolor='k', linewidth=0.5)
+            plt.text(day+0.48, (start + end) * 0.5, '{}\n({})'.format(lecture.lecture, lecture.professor), ha='center', va='center', fontsize=8)
+
+    ax = fig.add_subplot(111)
+    ylim = ax.get_ylim()
+
+    ax.yaxis.grid()
+    ax.set_xlim(0.5, 5.5)
+    ax.set_ylim(*ylim[::-1])
+    ax.set_xticks(range(1, 6))
+    ax.set_xticklabels(['월', '화', '수', '목', '금'])
+    ax.set_ylabel('Time')
+
+    ax2 = ax.twiny().twinx()
+    ax2.set_xlim(ax.get_xlim())
+    ax2.set_ylim(ax.get_ylim())
+    ax2.set_xticks(ax.get_xticks())
+    ax2.set_xticklabels(['월', '화', '수', '목', '금'])
+    ax2.set_ylabel('Time')
+
+    return fig
 
 
 def show_timetable_wizard():
@@ -25,10 +66,50 @@ def show_timetable_wizard():
     dist_level = st.select_slider(label='저는...', options=DIST_LEVEL_LABELS)
     dist_level = DIST_LEVEL_LABELS.index(dist_level)
 
+    blank_level = st.select_slider(label='공강은...', options=BLANK_LEVEL_LABELS)
+    blank_level = BLANK_LEVEL_LABELS.index(blank_level)
+
+    lecture_counts = st.slider(label='듣고 싶은 학점', value=[10, 21], max_value=30)
+
     lecture_list = LectureTimeDB()
     must_included = st.multiselect('이 과목은 꼭 들어야해요', lecture_list.get_lectures())
 
     if st.button('생성'):
         st.balloons()
 
-        st.write(difficulty_level, team_project, exam_level, dist_level)
+        evaluator = Evaluator(LectureList(), LectureReportDB())
+
+        timetabels = []
+
+        tt = TimeTable()
+        tt.add_lecture(lecture_list.get_timed_lecture_professor('시스템프로그래밍', '조영필')[0])
+        tt.add_lecture(lecture_list.get_timed_lecture_professor('알고리즘문제해결기법', '채동규')[0])
+        tt.add_lecture(lecture_list.get_timed_lecture('생명이란무엇인가?정신과물질')[0])
+        tt.add_lecture(lecture_list.get_timed_lecture_professor('소프트웨어실무영어', '오연주')[2])
+        tt.add_lecture(lecture_list.get_timed_lecture_professor('데이터베이스시스템및응용', '정형수')[0])
+        tt.add_lecture(lecture_list.get_timed_lecture_professor('컴퓨터네트워크', '이춘화')[0])
+        timetabels.append((tt, evaluator.evaluate(tt, difficulty_level, team_project, exam_level, dist_level, blank_level)))
+
+        tt = TimeTable()
+        tt.add_lecture(lecture_list.get_timed_lecture_professor('시스템프로그래밍', '조영필')[0])
+        tt.add_lecture(lecture_list.get_timed_lecture_professor('알고리즘문제해결기법', '채동규')[0])
+        tt.add_lecture(lecture_list.get_timed_lecture('생명이란무엇인가?정신과물질')[0])
+        tt.add_lecture(lecture_list.get_timed_lecture_professor('소프트웨어실무영어', '오연주')[1])
+        tt.add_lecture(lecture_list.get_timed_lecture_professor('데이터베이스시스템및응용', '정형수')[0])
+        tt.add_lecture(lecture_list.get_timed_lecture_professor('컴퓨터네트워크', '이춘화')[0])
+        timetabels.append((tt, evaluator.evaluate(tt, difficulty_level, team_project, exam_level, dist_level, blank_level)))
+
+        tt = TimeTable()
+        tt.add_lecture(lecture_list.get_timed_lecture_professor('시스템프로그래밍', '조영필')[0])
+        tt.add_lecture(lecture_list.get_timed_lecture_professor('알고리즘문제해결기법', '채동규')[0])
+        tt.add_lecture(lecture_list.get_timed_lecture('생명이란무엇인가?정신과물질')[0])
+        tt.add_lecture(lecture_list.get_timed_lecture_professor('소프트웨어실무영어', '오연주')[1])
+        tt.add_lecture(lecture_list.get_timed_lecture_professor('데이터베이스시스템및응용', '김상욱')[0])
+        tt.add_lecture(lecture_list.get_timed_lecture_professor('컴퓨터네트워크', '이춘화')[0])
+        timetabels.append((tt, evaluator.evaluate(tt, difficulty_level, team_project, exam_level, dist_level, blank_level)))
+
+        timetabels.sort(key=lambda x: x[1], reverse=True)
+
+        for tt, score in timetabels:
+            st.subheader('시간표1 (점수: {:.2f})'.format(score))
+            st.pyplot(draw_time_table(tt))
