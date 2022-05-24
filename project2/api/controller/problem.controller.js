1aa9feb54b71c00626242418cfc62143da60ffae
Junyeong Park <jyp10987@gmail.com>
2022-05-24 09:32:33 +0900

    feat(api): split problems by confidence

--- project2/api/controller/problem.controller.js
+++ project2/api/controller/problem.controller.js
@@ -1,9 +1,21 @@
-const problems = require('../database/problems.json');
+const lowProblems = require('../database/low_conf_problems.json');
+const midProblems = require('../database/mid_conf_problems.json');
+const highProblems = require('../database/high_conf_problems.json');
 const { wrapWithErrorHander } = require('../util');
 
 async function getProblem(req, res) {
-    const index = Math.floor(Math.random() * problems.length);
-    res.status(200).json(problems[index]);
+    if (req.query.level == 'low') {
+        const index = Math.floor(Math.random() * lowProblems.length);
+        res.status(200).json(lowProblems[index]);
+    } else if (req.query.level == 'mid') {
+        const index = Math.floor(Math.random() * midProblems.length);
+        res.status(200).json(midProblems[index]);
+    } else if (req.query.level == 'high') {
+        const index = Math.floor(Math.random() * highProblems.length);
+        res.status(200).json(highProblems[index]);
+    } else {
+        res.status(400).json({ error: 'invalid problem level '});
+    }
 }
 
 module.exports = wrapWithErrorHander({
