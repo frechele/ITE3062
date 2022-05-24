1c4a80112881eb2f129827445d96759f928fddf0
Junyeong Park <jyp10987@gmail.com>
2022-05-24 20:36:09 +0900

    feat(api): remove mid database

--- project2/api/controller/problem.controller.js
+++ project2/api/controller/problem.controller.js
@@ -1,5 +1,4 @@
 const lowProblems = require('../database/low_conf_problems.json');
-const midProblems = require('../database/mid_conf_problems.json');
 const highProblems = require('../database/high_conf_problems.json');
 const { wrapWithErrorHander } = require('../util');
 
@@ -7,9 +6,6 @@ async function getProblem(req, res) {
     if (req.query.level == 'low') {
         const index = Math.floor(Math.random() * lowProblems.length);
         res.status(200).json(lowProblems[index]);
-    } else if (req.query.level == 'mid') {
-        const index = Math.floor(Math.random() * midProblems.length);
-        res.status(200).json(midProblems[index]);
     } else if (req.query.level == 'high') {
         const index = Math.floor(Math.random() * highProblems.length);
         res.status(200).json(highProblems[index]);
